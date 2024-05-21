a2d24cd3d7f73b8d56313aa97f088850a6de2b72
Damian <damia_molin@hotmail.com>
2024-05-21 11:15:42 +0200

    subo archivos

---
+++ blog/urls.py
@@ -0,0 +1,47 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from categories.api.router import router_categories
+from posts.api.router import router_posts
+from comments.api.router import router_comments
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Blog API",
+      default_version='v1',
+      description="",
+      terms_of_service="",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   # permission_classes=(permissions.AllowAny,),
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redocs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('api/', include('users.api.router')),
+    path('api/', include(router_categories.urls)),
+    path('api/', include(router_posts.urls)),
+    path('api/', include(router_comments.urls))
+]
